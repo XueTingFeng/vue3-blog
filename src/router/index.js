d3e38454b28817b93513e6d19748c28503abfd50
xuetingfeng <coderxue@qq.com>
2022-04-07 16:24:24 +0800

    project init

--- src/router/index.js
+++ src/router/index.js
@@ -3,13 +3,16 @@ import {
   createWebHistory,
   createWebHashHistory,
 } from "vue-router";
-
-import HelloWorld from "../components/HelloWorld.vue";
+const HelloWorld = () => import("../components/HelloWorld.vue");
+//import HelloWorld from "../components/HelloWorld.vue";
 
 // 2. 定义一些路由
 // 每个路由都需要映射到一个组件。
 // 我们后面再讨论嵌套路由。
-const routes = [{ path: "/", component: HelloWorld }];
+const routes = [
+  { path: "/", component: HelloWorld },
+  { redirect: "/", path: "/HelloWorld" },
+];
 
 // 3. 创建路由实例并传递 `routes` 配置
 // 你可以在这里输入更多的配置，但我们在这里
